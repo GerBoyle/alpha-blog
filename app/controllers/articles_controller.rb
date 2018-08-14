7fa84b04e9319f517810ebbab5c9e0d6996fe5e3
Layout example <x16137108@ncirl.ie>
2018-08-14 16:14:01 +0000

    Update current user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,9 +19,8 @@ class ArticlesController < ApplicationController
     
     
     def create
-        #debugger
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
             flash[:success] = "Aritlce was successfully created"
             redirect_to article_path(@article)
